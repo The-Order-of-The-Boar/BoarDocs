310dda1224acc0fe31c5ba2a437df107c8f3eca7
Arthur Bacci <arthurbacci@protonmail.com>
2024-01-25 18:57:11 -0300

    better formatted license and pandoc demo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
+import Data.Text (Text)
+import Text.Pandoc.Readers (Reader, readers)
+import Text.Pandoc.Class (PandocIO, runIO)
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = print $ fmap fst (readers :: [(Text, Reader PandocIO)])
