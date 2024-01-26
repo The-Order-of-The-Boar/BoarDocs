87440685e619ce38558f32451651764f500f6a1e
Arthur Bacci <arthurbacci@protonmail.com>
2024-01-25 23:32:00 -0300

    Add Cfg and comments

--- app/Main.hs
+++ app/Main.hs
@@ -20,25 +20,51 @@ import Lib
 getPandocReader :: PandocMonad m => Text -> Maybe (Reader m)
 getPandocReader k = listToMaybe $ fmap snd $ filter ((== k) . fst) readers
 
-
-data CodeType = Lit | Code
+-- Lit (Literature)    for comments that will be rendered
+-- Code                for code, that will also be rendered (in a code block)
+-- Cfg (Configuration) for settings about the behaviour of Boar Docs or sets
+--                     some different kind of behaviour
+data CodeType = Lit | Code | Cfg
   deriving (Show, Eq)
 
+-- Organizing into chunks makes it easier to work with
 type Chunk = (CodeType, Text)
 
+litPrefix :: Text
+litPrefix = "////"
+cfgPrefix :: Text
+cfgPrefix = "///!"
+
+-- True if the chunks have the same CodeType
 sameCodeType :: Chunk -> Chunk -> Bool
 sameCodeType a b = fst a == fst b
 
+-- Separates a Text into various chunks
 separateText :: Text -> [Chunk]
-separateText t = fmap flatten $ NE.groupBy sameCodeType
-               $ fmap separateText' $ T.lines t
+               -- Concatenates (with newlines between) the adjacent chunks
+               -- (chunks in the same inner array)
+               -- [[(Lit, "a"), (Lit, "b")], [(Code, "a")]]
+               -- -> [(Lit, "a\nb"), (Code, "a")]
+separateText t = fmap flatten
+               -- Individual chunks with the same CodeType get grouped together
+               -- [Lit, Lit, Code] -> [[Lit, Lit], [Code]]
+               -- NE garantees that none of the inner lists are empty
+               $ NE.groupBy sameCodeType
+               -- Transforms the line into a chunk (decides the type)
+               $ fmap toChunk
+               -- Split the lines
+               $ T.lines t
   where
-    separateText' :: Text -> Chunk
-    separateText' s
-      | "///" `T.isPrefixOf` s = (Lit , T.drop 3 s)
-      | otherwise              = (Code, s)
+    toChunk :: Text -> Chunk
+    toChunk s
+      | litPrefix `T.isPrefixOf` s = (Lit, T.drop (T.length litPrefix) s)
+      | cfgPrefix `T.isPrefixOf` s = (Cfg, T.drop (T.length cfgPrefix) s)
+      | otherwise = (Code, s)
     flatten :: NonEmpty Chunk -> Chunk
+                     -- Gets the type of the first chunk (assuming that all the
+                     -- chunks are the same type)
     flatten chunks = let tp = fst $ NE.head chunks
+                     -- Joins the Texts of each chunk
                      in  (tp, T.unlines $ fmap snd $ NE.toList chunks)
     
 
@@ -52,7 +78,8 @@ main :: IO ()
 main = do
   arg <- fmap singleArgument getArgs
   text <- TIO.readFile arg
-  void $ sequence $ fmap print $ separateText $ text
+  let chunks = separateText $ text
+  void $ sequence $ fmap (TIO.putStrLn . snd) chunks
 
 
 
