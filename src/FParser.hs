97d52b585de1546c6220a2ac450e40ec4fb1e826
Arthur Bacci <arthurbacci@protonmail.com>
2024-01-30 23:38:00 -0300

    documentation

--- src/FParser.hs
+++ src/FParser.hs
@@ -34,46 +34,67 @@ type FParser = ParsecT Void Text (State FState)
 parseFile :: FParser ()
 parseFile = void $ many parseLine
 
+-- A single line, either Literature (markup), Config (metadata) or Code (the
+-- code itself)
 parseLine :: FParser ()
 parseLine = do
   r <- parseLiterature <|> parseConfig <|> parseCodeLine
   void eol
   return r
 
+-- Parses a line of literature/markup
 parseLiterature :: FParser ()
 parseLiterature = do
   _ <- string "////"
   void tillLineEnd
 
+-- Parses a line of code
 parseCodeLine :: FParser ()
 parseCodeLine = void $ tillLineEnd
 
+-- Parses a config line ("///!")
 parseConfig :: FParser ()
 parseConfig = do
   _ <- string "///!"
+  -- tries to parse a markup definition or throws an error while skipping the
+  -- line
   parseSetMarkup <|> tillLineEndFail "valid config"
 
+-- Modifies the configuration, setting the markup
+-- In case of invalid markup, it reports an error but doesn't fail
 parseSetMarkup :: FParser ()
 parseSetMarkup = do
+  -- default prefix for markup definition
   _ <- string "boar set markup "
+  -- no trimming is being done
   m <- tillLineEnd
   case getPandocTxtReader m of
     Just r -> do
+      -- modify the configuration, setting the new markup
       lift $ modify (\s -> s { bCfg = (bCfg s) { markup = Just (m, r) } })
     Nothing -> do
+      -- fails if the markup wasn't found
       betterFailureReg (T.unpack m) "valid markup"
 
 
+-- Returns the rest of the line as a string, but not consuming the eol
+-- Usually you will prefer to use tillLineEnd >>= eol
 tillLineEnd :: FParser Text
 tillLineEnd = do
   s <- takeWhileP Nothing (\x -> not $ x `elem` ("\r\n" :: String))
   return s
 
+-- Fails showing the rest of the line as unexpected input and the String
+-- argument as the label of the expected one
 tillLineEndFail :: String -> FParser ()
 tillLineEndFail s = do
   rest <- tillLineEnd
   betterFailureReg (T.unpack rest) s
 
+-- Wrapper to make registerFailure easier to use, by restricting the types
+-- The first string is the unexpected input, second is a label describing what
+-- was expected
+-- Any of them can be an empty string
 betterFailureReg :: String -> String -> FParser ()
 betterFailureReg u e = registerFailure un ex
   where
