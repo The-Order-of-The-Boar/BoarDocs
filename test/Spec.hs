6101caf11fb387feb9ed2571a57b820a10370205
LeonardoCBoar <leonardodicredico@gmail.com>
2024-01-31 00:50:45 -0300

    feat: Added unit test structure and tests for sameCodeType

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+module Main where
+import Test.HUnit
+import Chunks
+import Data.Text
+import qualified System.Exit as Exit
+ 
+sameCodeTypeLit:: Test
+sameCodeTypeLit = TestCase (assertEqual "Lit should be equal Lit" True (sameCodeType (Lit, pack("a")) (Lit, pack("b")) ) )
+
+sameCodeTypeCfg:: Test
+sameCodeTypeCfg = TestCase (assertEqual "Cfg should be equal Cfg" True (sameCodeType (Cfg, pack("b")) (Cfg, pack("a")) ) )
+
+sameCodeTypeCode:: Test
+sameCodeTypeCode = TestCase (assertEqual "Code should be equal Code" True (sameCodeType (Code, pack("d")) (Code, pack("e")) ) )
+ 
+sameCodeTypeTests :: Test
+sameCodeTypeTests = TestList [TestLabel "Lit" sameCodeTypeLit, TestLabel "Cfg" sameCodeTypeCfg, TestLabel "Code" sameCodeTypeCode]
+ 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    result <- runTestTT sameCodeTypeTests
+    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
